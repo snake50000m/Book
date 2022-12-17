901450231e6129c9864ce5afab2a8b615e069774
Danill <protopopov.danill@mail.ru>
2022-12-17 18:09:11 +0600

    update2

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,10 @@ public class Main {
         Book holm = new Book("Artur Conan Doyle", "Sherlock Holms");
 
         Reader student = new Reader("Данилл Протопопов", 343513452345L, "ЕТФ", "02.12.2002", "996500266744");
+        Reader student2 = new Reader("Хасан Нурубулоев", 343513452345L, "ЕТФ", "02.12.2002", "996500266744");
         student.takeBook(3);
         student.takeBook(new Book[]{hp, eo, holm});
+
+        student2.takeBook(4);
     }
 }
