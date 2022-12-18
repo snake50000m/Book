f4436c7a00c053dd9a172e45871fd6cfaff09baf
Danill <protopopov.danill@mail.ru>
2022-12-18 21:34:20 +0600

    update2

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,19 @@ public class Main {
         Book hp = new Book("J.Rouling", "Harry Potter");
         Book eo = new Book("A.Pushkin", "Evgenii Onegin");
         Book holm = new Book("Artur Conan Doyle", "Sherlock Holms");
+        String[] bookNames = new String[]{"Harry Potter", "Evgenii Onegin", "Sherlock Holms"};
 
         Reader student = new Reader("Данилл Протопопов", 343513452345L, "ЕТФ", "02.12.2002", "996500266744");
-        Reader student2 = new Reader("Хасан Нурубулоев", 343513452345L, "ЕТФ", "02.12.2002", "996500266744");
+        Reader student2 = new Reader("Хасан Нурубулоев", 343513452346L, "ЕТФ", "03.12.2002", "996707931678");
         student.takeBook(3);
+        student.takeBook(bookNames);
         student.takeBook(new Book[]{hp, eo, holm});
 
-        student2.takeBook(4);
+        System.out.println();
+
+        student.returnBook(2);
+        student.returnBook(new String[]{"Harry Potter", "Evgenii Onegin"});
+        student.returnBook(new Book[]{hp, eo});
+
     }
 }
--- src/models/Reader.java
+++ src/models/Reader.java
@@ -69,11 +69,57 @@ public class Reader {
             System.out.printf("Студент %s из факультета %s взял %d книг\n", fio, faculty, bookCount);
         }
     }
+
+    public void takeBook(String[] booksName){
+        if(booksName.length==1){
+            System.out.printf("Студент %s из факультета %s взял книгу %s \n", fio, faculty, booksName[0]);
+        }else {
+            System.out.printf("Студент %s из факультета %s взял книги", fio, faculty);
+            for (String a: booksName) {
+                System.out.print(" " + a + ",");
+            }
+            System.out.println();
+
+        }
+    }
+
     public void takeBook(Book[] books){
         if(books.length==1){
             System.out.printf("Студент %s из факультета %s взял книгу %s \n", fio, faculty, books[0]);
         }else {
             System.out.printf("Студент %s из факультета %s взял книги %s \n", fio, faculty, Arrays.toString(books).substring(1, Arrays.toString(books).length()-1));
+
+        }
+    }
+
+    public void returnBook(int bookCount) {
+        if (bookCount == 1) {
+            System.out.printf("Студент %s из факультета %s вернул %d книгу\n", fio, faculty, bookCount);
+        } else if (bookCount > 1 && bookCount < 5) {
+            System.out.printf("Студент %s из факультета %s вернул %d книги\n", fio, faculty, bookCount);
+        }else{
+            System.out.printf("Студент %s из факультета %s вернул %d книг\n", fio, faculty, bookCount);
+        }
+    }
+
+    public void returnBook(String[] booksName){
+        if(booksName.length==1){
+            System.out.printf("Студент %s из факультета %s вернул книгу %s \n", fio, faculty, booksName[0]);
+        }else {
+            System.out.printf("Студент %s из факультета %s вернул книги", fio, faculty);
+            for (String a: booksName) {
+                System.out.print(" " + a + ",");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public void returnBook(Book[] books){
+        if(books.length==1){
+            System.out.printf("Студент %s из факультета %s вернул книгу %s \n", fio, faculty, books[0]);
+        }else {
+            System.out.printf("Студент %s из факультета %s вернул книги %s \n", fio, faculty, Arrays.toString(books).substring(1, Arrays.toString(books).length()-1));
 
         }
     }
